9a3471a9debcf4bb4c0d784d49072af13db3da5a
Ahvrileuh <40494366+Ahvrileuh@users.noreply.github.com>
2019-07-01 14:08:21 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -38,11 +38,6 @@ clientDiscord.login(process.env.TOKENEPI);
 		   
 		if(on == true){
 
-			if(message.content === prefix + "help"){
-				message.author.send("DANS LE SALON bot-quiz : **!run Kuiz** ou **!run Kuiz + numero de la queston**"
-									+"DANS LE SALON commandes-bots : **!noraj**, **!ban**, **!dice**, **!ploufplouf**, **!echo**");
-			}
-
 			if(message.content === prefix + "nsfw"){
 				
 				message.channel.send("c'est pas gentil d'être mechant avec les brigands 🍆");
@@ -144,38 +139,24 @@ clientDiscord.login(process.env.TOKENEPI);
 				}
 			}
 			
-			/*if(message.content === perfix + "help"){
+			if(message.content === perfix + "help"){
 				
 			   message.channel.send({embed: {
-    color: 3447003,
-    author: {
-      name: client.user.username,
-      icon_url: client.user.avatarURL
-    },
-    title: "This is an embed",
-    url: "http://google.com",
-    description: "This is a test embed to showcase what they look like and what they can do.",
-    fields: [{
-        name: "Fields",
-        value: "They can have different fields with small headlines."
-      },
-      {
-        name: "Masked links",
-        value: "You can put [masked links](http://google.com) inside of rich embeds."
-      },
-      {
-        name: "Markdown",
-        value: "You can put all the *usual* **__Markdown__** inside of them."
-      }
-    ],
-    timestamp: new Date(),
-    footer: {
-      icon_url: client.user.avatarURL,
-      text: "© Example"
-    }
-  }
-});
-*/
+				   color: 3447003,
+				   description: "test",
+				   fields: [
+					   {
+					   	name: "test",
+						value: "test"
+					   },
+					   {
+					   	name: "test",
+						value: "test"
+					   }
+				   ],
+			   }
+			});
+			}
 			if(message.content === prefix + "pin"){
 			
 				//message.channel.send("**VOTRE PROCHAIN MESSAGE SERA EPINGLER**");
