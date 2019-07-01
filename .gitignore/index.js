e65e56a2bcdcc90bd42fdf40ab19a3dc09cbc413
Ahvrileuh <40494366+Ahvrileuh@users.noreply.github.com>
2019-07-01 12:13:07 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -146,27 +146,35 @@ clientDiscord.login(process.env.TOKENEPI);
 			
 			if(message.content === perfix + "help"){
 				
-			   message.channel.send({
-			   	embed{
-			   		color: 3447003,
-			   		description: "**TEST**",
-			   		fields: [
-			   			
-			   		{
-			   			name: "test",
-			   			value: "testestetststetstdtdd",
-			   		},
-			   
-			   		{
-			   			name: "test",
-			   			value: "testestetststetstdtdd",
-			   		}
-			   			
-			   		],
-			   	}
-			   });
-			   
-			}
+			   message.channel.send({embed: {
+    color: 3447003,
+    author: {
+      name: client.user.username,
+      icon_url: client.user.avatarURL
+    },
+    title: "This is an embed",
+    url: "http://google.com",
+    description: "This is a test embed to showcase what they look like and what they can do.",
+    fields: [{
+        name: "Fields",
+        value: "They can have different fields with small headlines."
+      },
+      {
+        name: "Masked links",
+        value: "You can put [masked links](http://google.com) inside of rich embeds."
+      },
+      {
+        name: "Markdown",
+        value: "You can put all the *usual* **__Markdown__** inside of them."
+      }
+    ],
+    timestamp: new Date(),
+    footer: {
+      icon_url: client.user.avatarURL,
+      text: "© Example"
+    }
+  }
+});
 
 			if(message.content === prefix + "pin"){
 			
