5439d7f898faf87c144fb11f35548616a0df99e7
Ahvrileuh <40494366+Ahvrileuh@users.noreply.github.com>
2019-07-01 14:35:05 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -2,7 +2,7 @@
 const Discord = require('discord.js');
 const clientDiscord = new Discord.Client();
 
-var vers = "2";
+var vers = "3";
 
 var BAN = ['lepton', 'rat', 'test', 'test2'];
 BAN.lenght = 4;
